a228e33c6b49ad90335ad276237d02035c1c557f
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2026-01-17 18:55:55 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,12 @@
 import React from 'react'
-import Task1 from './components/statetask/Task1'
-import Task2 from './components/statetask/Task2'
-import Task3 from './components/statetask/Task3';
-import Task4 from './components/statetask/Task4';
-import Task5 from './components/statetask/Task5';
+import PropsTask1 from './Components/task/PropsTask1'
 
 const App = () => {
   return (
     <div>
-      <Task1></Task1>
-      <br />
-      <br />
-      <Task2></Task2>
-       <br />
-      <br />
-      <Task3></Task3>
-      <br />
-      <br />
-      <Task4></Task4>
-      <br />
-      <br />
-      <Task5></Task5>
+      <PropsTask1 num1 = {10} num2={20} />
     </div>
   )
 }
 
 export default App
-
---
+++ src/Components/task/PropsTask2.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropsTask3 from './PropsTask3'
+
+const PropsTask2 = ({num1 ,num2}) => {
+
+
+  return (
+    <div>
+    
+    <PropsTask3  num1 = {num1} num2={num2} />
+
+    </div>
+  )
+}
+
+export default PropsTask2
