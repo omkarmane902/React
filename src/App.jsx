380fba2f852aff06dc6e6946c0b439c50230fbf3
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2026-02-12 01:19:08 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import Theme from './ThemeContext/Theme'
-import ThemeContext from './ThemeContext/ThemeContext'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  let [count,setCount]=useState(0);
+  let handel = ()=>{
+    console.log("Button clicked....");
+    
+  }
   return (
     <div>
-         <div>
-              <ThemeContext>
-                        <Theme/>  
-              </ThemeContext>
-         </div>
+        <button onClick={()=>{setCount(count + 1)}}>Count:{count}</button><br /><br />
+        <button onClick={handel}>count</button>
     </div>
   )
 }
